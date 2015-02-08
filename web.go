79f73ee558bad46907a2b89fedf8346fdfb66a22
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2015-02-08 17:46:24 +0800

    static file server

---
+++ web.go
@@ -0,0 +1,23 @@
+package main 
+
+
+import (
+	"log"
+
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+
+func main() {
+	router := mux.NewRouter()
+
+	// static
+    router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
+    // register routes
+    http.Handle("/", router)
+
+    log.Println("Listening...")
+    http.ListenAndServe(":8080", nil)
+}
