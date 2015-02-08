0b885ff27428a762a5810fde93f7022277e9e744
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2015-02-08 21:41:23 +0800

    great basic histogram works now interface for data call from browser

--- public/js/barchart.js
+++ public/js/barchart.js
@@ -7,19 +7,25 @@
 var d3Chart = {};
 
 d3Chart.create = function(el, props, data) {
+	var margin = 30;
 	var svg = d3.select(el).append('svg')
 		.attr('class', 'd3')
 		.attr('width', props.width)
 		.attr('height', props.height);
 
-	var height = d3.select(el).node().getBoundingClientRect().height;
-	var width = d3.select(el).node().getBoundingClientRect().width;
+	var height = svg.node().getBoundingClientRect().height - margin * 2;
+	var width = svg.node().getBoundingClientRect().width - margin * 2;
 
+
+	// notice fake data concat to make scales nicer
 	var ymin = d3.min(data.concat([ { value: 0 }]), function(d) { return d.value; });
 	var ymax = d3.max(data, function(d) { return d.value; });
 	var yScale = d3.scale.linear()
 					.domain([ ymin, ymax ])
-					.range([ 0, height  ]);
+					.range([ 0 , height ]);
+	var yInvertedScale = d3.scale.linear()
+					.domain([ ymin, ymax])
+					.range([height , 0]);
 
 	var color = d3.scale.category20();
 
@@ -27,25 +33,24 @@ d3Chart.create = function(el, props, data) {
 	var rectWidth = width / (2 * data.length);
 
 
-	// add rects
+	// add groups
 	var groups = svg.selectAll("g")
 		.data(data)
 		.enter()
 		.append("g")
+		// extra margin/2 for scale
 		.attr("transform", function(d, i) {
-			return "translate(" + (rectWidth/2 + i * rectWidth * 2) + "," + (height - yScale(d.value)) + ")";
+			return "translate(" + (margin + rectWidth/2 + i * rectWidth * 2) + "," + (height + margin - yScale(d.value)) + ")";
+		})
+		.on('mouseover', function(d) {
+			d3.select(this).style("opacity", 0.7);
+		})
+		.on('mouseout', function(d) {
+			d3.select(this).style("opacity", 1);
 		});
 
-
+	// add rects
 	groups.append("rect")
-		// .attr("x", function(d, i) {
-		// 	return 0;
-		// 	//return rectWidth/2 + i * rectWidth * 2;
-		// })
-		// .attr("y", function(d) {
-		// 	return 0
-		// 	//return height - yScale(d.value);
-		// })
 		.attr("width", rectWidth)
 		.attr("height", function(d) {
 			return yScale(d.value);
@@ -53,6 +58,53 @@ d3Chart.create = function(el, props, data) {
 		.style("fill", function(d, i) {
 			return color(i);
 		});
+
+	// add label text
+	groups.append("text")
+		// .attr("dominant-baseline", "hanging")
+		.attr("x", rectWidth/2)
+		.attr("y", function(d) { 
+			return yScale(d.value) - margin/2; // margin arbitrary here
+		})
+		.style("text-anchor", "middle")
+		.text(function(d) {
+			return d.label;
+		})
+
+
+	// draw y axis
+	var yAxis = svg.append("g")
+		.attr("class", "y-axis")
+		.attr("fill", "none")
+	    .attr("stroke", "grey")
+	    .attr("stroke-width", 1)
+	    //.style("shape-rendering", "crispEdges")
+	    //.style("font-size", 10)
+		.attr("transform", function(d) {
+			return "translate(" + margin + "," + margin + ")";
+		})
+		.call(d3.svg.axis()
+	    	.scale(yInvertedScale)
+	    	.orient("right")
+	    	.ticks(5)
+	    	.tickSize(5, 0)
+	    );
+
+	// simple x axis line
+	var xAxis = svg.append("g")
+		.attr("class", "x-axis")
+		.attr("transform", function(d) {
+			// ! don't forget to flip y coordinate
+			return "translate(" + margin + "," + (height + margin) + ")";
+		});
+		
+	xAxis.append("line")
+		.attr("x1", 0)
+		.attr("y1", 0)
+		.attr("x2", width-margin/2)
+		.attr("y2", 0)
+		.attr("stroke", "lightgrey")
+		.attr("stroke-width", 1);
 };
 
 
@@ -68,9 +120,10 @@ var BarChart = React.createClass({
 
 	getInitialState: function() {
 		return { data: [
-			{ label: 'a', value: 10 },
-			{ label: 'b', value: 20 },
-			{ label: 'c', value: 30 }
+			{ label: 'Group A', value: 30 },
+			{ label: 'Group B', value: 5 },
+			{ label: 'Group C', value: 20 },
+			{ label: 'Group D', value: 10 }
 		] };
 	},
 
