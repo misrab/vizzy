dd43175dafed118883b447a8833336270e7a3957
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2015-02-10 14:02:22 +0800

    rewrote bar chart for better scaling inc negatives

--- public/js/barchart.js
+++ public/js/barchart.js
@@ -7,40 +7,55 @@
 var d3Chart = {};
 
 d3Chart.create = function(el, props, data) {
-	var margin = 30;
+	// margin for top, bottom, left, right
+	// i.e. empty space, in case something like text goes outside a bit
+	var margin = 15;
 	var svg = d3.select(el).append('svg')
 		.attr('class', 'd3')
 		.attr('width', props.width)
 		.attr('height', props.height);
 
+	// height and width of workable space i.e. without margins
 	var height = svg.node().getBoundingClientRect().height - margin * 2;
 	var width = svg.node().getBoundingClientRect().width - margin * 2;
 
-
-	// notice fake data concat to make scales nicer
-	var ymin = d3.min(data.concat([ { value: 0 }]), function(d) { return d.value; });
+	// the y-scale puts 0 in the right position i.e. the whole range is mapped to 0->height
+	var ymin = d3.min(data, function(d) { return d.value; });
 	var ymax = d3.max(data, function(d) { return d.value; });
 	var yScale = d3.scale.linear()
-					.domain([ ymin, ymax ])
-					.range([ 0 , height ]);
+		.domain([ ymin, ymax ])
+		.range([ 0 , height ]);
+	// inverted scale for y-axis
 	var yInvertedScale = d3.scale.linear()
-					.domain([ ymin, ymax])
-					.range([height , 0]);
+		.domain([ ymin, ymax])
+		.range([height , 0]);
+
+
+	// the scale for bars is based on absolute heights
+	var barmin = d3.min(data.concat([ { value: 0 }]), function(d) { return Math.abs(d.value); });
+	var barmax = d3.max(data, function(d) { return d.value; });
+	var barScale = d3.scale.linear()
+		.domain([ barmin, barmax ])
+		.range([ 0 , height - yScale(0) ]);
+
+
 
 	var color = d3.scale.category20();
-
 	// gaps == rect width, hence 2
 	var rectWidth = width / (2 * data.length);
 
 
-	// add groups
+
+
+	// add groups. y location 0 - barheight or + barheight if negative value
 	var groups = svg.selectAll("g")
 		.data(data)
 		.enter()
 		.append("g")
 		// extra margin/2 for scale
 		.attr("transform", function(d, i) {
-			return "translate(" + (margin + rectWidth/2 + i * rectWidth * 2) + "," + (height + margin - yScale(d.value)) + ")";
+			var barheight = d.value < 0 ? 0 : barScale(Math.abs(d.value)); //margin + height - barheight - yScale(0)
+			return "translate(" + (margin + rectWidth/2 + i * rectWidth * 2) + "," + (margin + height - yScale(0) - barheight) + ")";
 		})
 		.on('mouseover', function(d) {
 			d3.select(this).style("opacity", 0.7);
@@ -53,25 +68,14 @@ d3Chart.create = function(el, props, data) {
 	groups.append("rect")
 		.attr("width", rectWidth)
 		.attr("height", function(d) {
-			return yScale(d.value);
+			return barScale(Math.abs(d.value));
 		})
 		.style("fill", function(d, i) {
 			return color(i);
 		});
 
-	// add label text
-	groups.append("text")
-		// .attr("dominant-baseline", "hanging")
-		.attr("x", rectWidth/2)
-		.attr("y", function(d) { 
-			return yScale(d.value) - margin/2; // margin arbitrary here
-		})
-		.style("text-anchor", "middle")
-		.text(function(d) {
-			return d.label;
-		})
 
-
+	// draw the axes
 	// draw y axis
 	var yAxis = svg.append("g")
 		.attr("class", "y-axis")
@@ -95,9 +99,8 @@ d3Chart.create = function(el, props, data) {
 		.attr("class", "x-axis")
 		.attr("transform", function(d) {
 			// ! don't forget to flip y coordinate
-			return "translate(" + margin + "," + (height + margin) + ")";
-		});
-		
+			return "translate(" + margin + "," + (height - yScale(0) + margin) + ")";
+		});	
 	xAxis.append("line")
 		.attr("x1", 0)
 		.attr("y1", 0)
@@ -105,12 +108,106 @@ d3Chart.create = function(el, props, data) {
 		.attr("y2", 0)
 		.attr("stroke", "lightgrey")
 		.attr("stroke-width", 1);
+
+
+	// // notice fake data concat to make scales nicer
+	// var ymin = d3.min(data.concat([ { value: 0 }]), function(d) { return Math.abs(d.value); });
+	// var ymax = d3.max(data, function(d) { return d.value; });
+	// var yScale = d3.scale.linear()
+	// 				.domain([ ymin, ymax ])
+	// 				.range([ 0 , height ]);
+	// // not taking absolute values
+	// var trueymin = d3.min(data.concat([ { value: 0 }]), function(d) { return d.value; });
+	// var yInvertedScale = d3.scale.linear()
+	// 				.domain([ trueymin, ymax])
+	// 				.range([height , 0]);
+
+	// var color = d3.scale.category20();
+
+	// // gaps == rect width, hence 2
+	// var rectWidth = width / (2 * data.length);
+
+
+	// // add groups
+	// var groups = svg.selectAll("g")
+	// 	.data(data)
+	// 	.enter()
+	// 	.append("g")
+	// 	// extra margin/2 for scale
+	// 	.attr("transform", function(d, i) {
+	// 		return "translate(" + (margin + rectWidth/2 + i * rectWidth * 2) + "," + (margin + height - yScale(d.value) - yScale(0)) + ")";
+	// 	})
+	// 	.on('mouseover', function(d) {
+	// 		d3.select(this).style("opacity", 0.7);
+	// 	})
+	// 	.on('mouseout', function(d) {
+	// 		d3.select(this).style("opacity", 1);
+	// 	});
+
+	// // add rects
+	// groups.append("rect")
+	// 	.attr("width", rectWidth)
+	// 	.attr("height", function(d) {
+	// 		return yScale(d.value);
+	// 	})
+	// 	.style("fill", function(d, i) {
+	// 		return color(i);
+	// 	});
+
+	// // add label text
+	// groups.append("text")
+	// 	// .attr("dominant-baseline", "hanging")
+	// 	.attr("x", rectWidth/2)
+	// 	.attr("y", function(d) { 
+	// 		return yScale(d.value) - margin/2; // margin arbitrary here
+	// 	})
+	// 	.style("text-anchor", "middle")
+	// 	.text(function(d) {
+	// 		return d.label;
+	// 	})
+
+
+	// // draw y axis
+	// var yAxis = svg.append("g")
+	// 	.attr("class", "y-axis")
+	// 	.attr("fill", "none")
+	//     .attr("stroke", "grey")
+	//     .attr("stroke-width", 1)
+	//     //.style("shape-rendering", "crispEdges")
+	//     //.style("font-size", 10)
+	// 	.attr("transform", function(d) {
+	// 		return "translate(" + margin + "," + (margin + yScale(ymin)) + ")";
+	// 	})
+	// 	.call(d3.svg.axis()
+	//     	.scale(yInvertedScale)
+	//     	.orient("right")
+	//     	.ticks(5)
+	//     	.tickSize(5, 0)
+	//     );
+
+	// // simple x axis line
+	// var xAxis = svg.append("g")
+	// 	.attr("class", "x-axis")
+	// 	.attr("transform", function(d) {
+	// 		// ! don't forget to flip y coordinate
+	// 		return "translate(" + margin + "," + (height - yScale(0) + margin) + ")";
+	// 	});
+		
+	// xAxis.append("line")
+	// 	.attr("x1", 0)
+	// 	.attr("y1", 0)
+	// 	.attr("x2", width-margin/2)
+	// 	.attr("y2", 0)
+	// 	.attr("stroke", "lightgrey")
+	// 	.attr("stroke-width", 1);
 };
 
 
 d3Chart.destroy = function(el) {
 	el.remove();
 };
+
+
 
 /*
 	React
@@ -121,9 +218,9 @@ var BarChart = React.createClass({
 	getInitialState: function() {
 		return { data: [
 			{ label: 'Group A', value: 30 },
-			{ label: 'Group B', value: 5 },
-			{ label: 'Group C', value: 20 },
-			{ label: 'Group D', value: 10 }
+			{ label: 'Group B', value: 20 },
+			{ label: 'Group C', value: 10 },
+			{ label: 'Group D', value: -10 }
 		] };
 	},
 
@@ -156,39 +253,3 @@ React.render(
 	<BarChart />,
 	document.getElementById('charts')
 );
-
-
-
-
-
-
-// d3Chart.update = function(el, state) {
-//   // Re-compute the scales, and render the data points
-//   var scales = this._scales(el, state.domain);
-//   this._drawPoints(el, scales, state.data);
-// };
-
-// d3Chart.destroy = function(el) {
-//   // Any clean-up would go here
-//   // in this example there is nothing to do
-// };
-
-// d3Chart._drawPoints = function(el, scales, data) {
-//   var g = d3.select(el).selectAll('.d3-points');
-
-//   var point = g.selectAll('.d3-point')
-//     .data(data, function(d) { return d.id; });
-
-//   // ENTER
-//   point.enter().append('circle')
-//       .attr('class', 'd3-point');
-
-//   // ENTER & UPDATE
-//   point.attr('cx', function(d) { return scales.x(d.x); })
-//       .attr('cy', function(d) { return scales.y(d.y); })
-//       .attr('r', function(d) { return scales.z(d.z); });
-
-//   // EXIT
-//   point.exit()
-//       .remove();
-// };
