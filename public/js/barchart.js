1011e0ea14c0598a3c928026fc0b860c58144000
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2015-02-10 14:11:34 +0800

    rewrote bar chart for better scaling inc negatives

--- public/js/barchart.js
+++ public/js/barchart.js
@@ -75,6 +75,20 @@ d3Chart.create = function(el, props, data) {
 		});
 
 
+
+	// add label text
+	groups.append("text")
+		// .attr("dominant-baseline", "hanging")
+		.attr("x", rectWidth/2)
+		.attr("y", function(d) { 
+			return barScale(Math.abs(d.value)) * 0.5; // margin arbitrary here
+		})
+		.style("text-anchor", "middle")
+		.text(function(d) {
+			return d.label;
+		})
+
+
 	// draw the axes
 	// draw y axis
 	var yAxis = svg.append("g")
@@ -109,97 +123,6 @@ d3Chart.create = function(el, props, data) {
 		.attr("stroke", "lightgrey")
 		.attr("stroke-width", 1);
 
-
-	// // notice fake data concat to make scales nicer
-	// var ymin = d3.min(data.concat([ { value: 0 }]), function(d) { return Math.abs(d.value); });
-	// var ymax = d3.max(data, function(d) { return d.value; });
-	// var yScale = d3.scale.linear()
-	// 				.domain([ ymin, ymax ])
-	// 				.range([ 0 , height ]);
-	// // not taking absolute values
-	// var trueymin = d3.min(data.concat([ { value: 0 }]), function(d) { return d.value; });
-	// var yInvertedScale = d3.scale.linear()
-	// 				.domain([ trueymin, ymax])
-	// 				.range([height , 0]);
-
-	// var color = d3.scale.category20();
-
-	// // gaps == rect width, hence 2
-	// var rectWidth = width / (2 * data.length);
-
-
-	// // add groups
-	// var groups = svg.selectAll("g")
-	// 	.data(data)
-	// 	.enter()
-	// 	.append("g")
-	// 	// extra margin/2 for scale
-	// 	.attr("transform", function(d, i) {
-	// 		return "translate(" + (margin + rectWidth/2 + i * rectWidth * 2) + "," + (margin + height - yScale(d.value) - yScale(0)) + ")";
-	// 	})
-	// 	.on('mouseover', function(d) {
-	// 		d3.select(this).style("opacity", 0.7);
-	// 	})
-	// 	.on('mouseout', function(d) {
-	// 		d3.select(this).style("opacity", 1);
-	// 	});
-
-	// // add rects
-	// groups.append("rect")
-	// 	.attr("width", rectWidth)
-	// 	.attr("height", function(d) {
-	// 		return yScale(d.value);
-	// 	})
-	// 	.style("fill", function(d, i) {
-	// 		return color(i);
-	// 	});
-
-	// // add label text
-	// groups.append("text")
-	// 	// .attr("dominant-baseline", "hanging")
-	// 	.attr("x", rectWidth/2)
-	// 	.attr("y", function(d) { 
-	// 		return yScale(d.value) - margin/2; // margin arbitrary here
-	// 	})
-	// 	.style("text-anchor", "middle")
-	// 	.text(function(d) {
-	// 		return d.label;
-	// 	})
-
-
-	// // draw y axis
-	// var yAxis = svg.append("g")
-	// 	.attr("class", "y-axis")
-	// 	.attr("fill", "none")
-	//     .attr("stroke", "grey")
-	//     .attr("stroke-width", 1)
-	//     //.style("shape-rendering", "crispEdges")
-	//     //.style("font-size", 10)
-	// 	.attr("transform", function(d) {
-	// 		return "translate(" + margin + "," + (margin + yScale(ymin)) + ")";
-	// 	})
-	// 	.call(d3.svg.axis()
-	//     	.scale(yInvertedScale)
-	//     	.orient("right")
-	//     	.ticks(5)
-	//     	.tickSize(5, 0)
-	//     );
-
-	// // simple x axis line
-	// var xAxis = svg.append("g")
-	// 	.attr("class", "x-axis")
-	// 	.attr("transform", function(d) {
-	// 		// ! don't forget to flip y coordinate
-	// 		return "translate(" + margin + "," + (height - yScale(0) + margin) + ")";
-	// 	});
-		
-	// xAxis.append("line")
-	// 	.attr("x1", 0)
-	// 	.attr("y1", 0)
-	// 	.attr("x2", width-margin/2)
-	// 	.attr("y2", 0)
-	// 	.attr("stroke", "lightgrey")
-	// 	.attr("stroke-width", 1);
 };
 
 
